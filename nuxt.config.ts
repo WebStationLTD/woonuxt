1841033b863004179133b109980adee7af503115
nikiforBG <killtechbg@gmail.com>
2025-05-07 21:33:03 +0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,13 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   extends: ["./woonuxt_base"],
+
   components: [{ path: "./components", pathPrefix: false }],
+
   modules: ["nuxt-graphql-client", "@nuxtjs/sitemap"],
 
-
-
-
+  experimental: {
+    payloadExtraction: true,
+  },
 
   runtimeConfig: {
     public: {
@@ -26,7 +27,7 @@ export default defineNuxtConfig({
   },
 
   sitemap: {
-    siteUrl: process.env.SITE_URL || "https://woonuxt-shop.admin-panels.com",
+    siteUrl: "https://woonuxt-shop.admin-panels.com",
     excludes: [
       "/checkout/order-received/**",
       "/order-summary/**",
@@ -55,8 +56,37 @@ export default defineNuxtConfig({
 
   nitro: {
     prerender: {
+      routes: ["/", "/products", "/categories", "/contact"],
       concurrency: 10,
       interval: 1000,
       failOnError: false,
     },
     minify: true,
+    routeRules: {
+      // Генерирани по време на билд
+      "/": { static: true },
+      "/products": { static: true },
+      "/categories": { static: true },
+      "/contact": { static: true },
+
+      // Частично кеширани с ISR (Incremental Static Regeneration)
+      "/product/**": {
+        isr: {
+          expiration: 600, // 10 минути
+        },
+      },
+      "/produkt-kategoriya/**": {
+        isr: {
+          expiration: 600,
+        },
+      },
+
+      // Страници с SSR, без кеш
+      "/checkout/**": { ssr: true, cache: false },
+      "/cart": { ssr: true, cache: false },
+      "/my-account/**": { ssr: true, cache: false },
+    },
+  },
+
+  compatibilityDate: "2025-05-03",
+});
