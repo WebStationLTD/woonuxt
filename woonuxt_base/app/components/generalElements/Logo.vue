b9dcfe68008478fbf9b172982424eb6a0c756095
7onc1 <anton.demirev84@gmail.com>
2025-05-08 00:17:45 +0300

    Add logo and fix menu paddings

--- woonuxt_base/app/components/generalElements/Logo.vue
+++ woonuxt_base/app/components/generalElements/Logo.vue
@@ -4,15 +4,15 @@ const runtimeConfig = useRuntimeConfig();
 const img = useImage();
 
 const logoUrl = runtimeConfig?.public?.LOGO ? img(runtimeConfig?.public?.LOGO) : null;
-const faviconUrl = '/logo.svg';
+const faviconUrl = '/next-level-logo.png';
 </script>
 
 <template>
   <NuxtLink to="/" class="inline-flex items-center gap-2">
     <img v-if="logoUrl" :src="logoUrl" alt="Logo" class="object-contain h-10" />
     <div v-else class="flex items-center gap-2 text-lg font-bold">
-      <img :src="faviconUrl" alt="Logo" width="32" height="32" />
-      <span>{{ siteName }}</span>
+      <img :src="faviconUrl" alt="Logo" width="180" height="40" />
+      <!-- <span>{{ siteName }}</span> -->
     </div>
   </NuxtLink>
 </template>
