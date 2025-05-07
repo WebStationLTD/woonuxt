61641dccf1360a8330d7534a888fc719074a4924
7onc1 <anton.demirev84@gmail.com>
2025-05-07 23:58:20 +0300

    Add top header bar

--- woonuxt_base/app/components/generalElements/AppHeader.vue
+++ woonuxt_base/app/components/generalElements/AppHeader.vue
@@ -3,6 +3,7 @@ const { isShowingSearch } = useSearching();
 </script>
 
 <template>
+  <InfoTopBar />
   <header class="sticky top-0 z-40 bg-white shadow-sm shadow-light-500">
     <div class="flex w-full items-center justify-between py-4 px-4">
       <div class="flex items-center">
