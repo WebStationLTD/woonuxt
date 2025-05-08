f2c1269fd0ed6d7967e7d0d680ea0d82f48b1257
7onc1 <anton.demirev84@gmail.com>
2025-05-08 10:33:23 +0300

    Change favicon

--- woonuxt_base/nuxt.config.ts
+++ woonuxt_base/nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
     head: {
       htmlAttrs: { lang: 'bg' },
       link: [
-        { rel: 'icon', href: '/logo.svg', type: 'image/svg+xml' },
+        { rel: 'icon', href: '/next-level-logo.png', type: 'image/png' },
         { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.4/tiny-slider.css' },
       ],
       script: [
