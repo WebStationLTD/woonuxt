584480411495ae56292c831dda92b1dd8068c78c
7onc1 <anton.demirev84@gmail.com>
2025-05-08 22:26:45 +0300

    Change number of categories fetch in homepage

--- woonuxt_base/app/pages/index.vue
+++ woonuxt_base/app/pages/index.vue
@@ -11,7 +11,7 @@ const { data: seoData } = await useAsyncGql('getHomeSeo');
 const homeSeo = seoData.value?.page?.seo || null;
 
 // Получаване на категории
-const { data } = await useAsyncGql('getProductCategories', { first: 6 });
+const { data } = await useAsyncGql('getProductCategories', { first: 10 });
 const productCategories = data.value?.productCategories?.nodes || [];
 
 // Получаване на популярни продукти
@@ -58,7 +58,7 @@ if (homeSeo?.schema?.raw) {
         <h2 class="text-lg font-semibold md:text-2xl">{{ $t('messages.shop.shopByCategory') }}</h2>
         <NuxtLink class="text-primary" to="/categories">{{ $t('messages.general.viewAll') }}</NuxtLink>
       </div>
-      <div class="grid justify-center grid-cols-2 gap-4 mt-8 md:grid-cols-3 lg:grid-cols-6">
+      <div class="grid justify-center grid-cols-2 gap-4 mt-8 md:grid-cols-3 lg:grid-cols-5">
         <CategoryCard v-for="(category, i) in productCategories" :key="i" class="w-full" :node="category" />
       </div>
     </section>
