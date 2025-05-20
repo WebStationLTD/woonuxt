7bb89c2437bbb07df4458ada445fd1a8df9f4ea6
7onc1 <anton.demirev84@gmail.com>
2025-05-20 10:44:01 +0300

    Fix product cards filter height issue

--- woonuxt_base/app/components/shopElements/ProductGrid.vue
+++ woonuxt_base/app/components/shopElements/ProductGrid.vue
@@ -25,9 +25,9 @@ const productsToShow = computed(() => products.value.slice((page.value - 1) * pr
 
 <style lang="postcss" scoped>
 .product-grid {
-  @apply my-4 min-h-[600px] grid transition-all gap-[0.7rem] sm:gap-8 lg:my-8;
-
+  @apply my-4 grid transition-all gap-[0.7rem] sm:gap-8 lg:my-8;
   grid-template-columns: repeat(2, 1fr);
+  grid-auto-rows: min-content;
 }
 .product-grid:empty {
   display: none;
