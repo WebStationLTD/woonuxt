053dc6ae9c3325b5645bcbddbe46b31a6450a810
7onc1 <anton.demirev84@gmail.com>
2025-06-03 17:20:42 +0300

    app.leaderfitness.net - change admin panel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      GQL_HOST: "https://woonuxt-shop.admin-panels.com/graphql",
+      GQL_HOST: "https://app.leaderfitness.net/graphql",
       PRODUCT_CATEGORY_PERMALINK: "/produkt-kategoriya/",
       PRODUCTS_PER_PAGE: 12,
     },
@@ -20,14 +20,14 @@ export default defineNuxtConfig({
   app: {
     head: {
       link: [
-        { rel: "preconnect", href: "https://woonuxt-shop.admin-panels.com" },
-        { rel: "dns-prefetch", href: "https://woonuxt-shop.admin-panels.com" },
+        { rel: "preconnect", href: "https://app.leaderfitness.net" },
+        { rel: "dns-prefetch", href: "https://app.leaderfitness.net" },
       ],
     },
   },
 
   sitemap: {
-    siteUrl: "https://woonuxt-shop.admin-panels.com",
+    siteUrl: "https://app.leaderfitness.net",
     excludes: [
       "/checkout/order-received/**",
       "/order-summary/**",
@@ -41,7 +41,7 @@ export default defineNuxtConfig({
   "graphql-client": {
     clients: {
       default: {
-        host: "https://woonuxt-shop.admin-panels.com/graphql",
+        host: "https://app.leaderfitness.net/graphql",
         tokenStorage: {
           cookieOptions: {
             name: "authToken",
