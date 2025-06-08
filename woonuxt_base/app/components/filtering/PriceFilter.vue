045603abe2d6f017bed72c1e8a484596a0543056
7onc1 <anton.demirev84@gmail.com>
2025-06-08 23:26:07 +0300

    Fix $ to лв.

--- woonuxt_base/app/components/filtering/PriceFilter.vue
+++ woonuxt_base/app/components/filtering/PriceFilter.vue
@@ -4,7 +4,7 @@ import Slider from '@vueform/slider';
 const { getFilter, setFilter, isFiltersActive } = useFiltering();
 const runtimeConfig = useRuntimeConfig();
 const maxPrice = runtimeConfig?.public?.MAX_PRICE || 1000;
-const currencySymbol = runtimeConfig?.public?.CURRENCY_SYMBOL || '$';
+const currencySymbol = runtimeConfig?.public?.CURRENCY_SYMBOL || ' лв.';
 
 const activeFilters = ref(getFilter('price'));
 const price = activeFilters.value.length ? ref(activeFilters.value) : ref([0, maxPrice]);
@@ -34,21 +34,21 @@ watch(isFiltersActive, () => {
         <input
           id="price-from"
           v-model="price[0]"
-          class="bg-white border rounded-lg max-w-full border-gray-200 leading-none w-auto p-2 pl-6 md:text-sm"
+          class="bg-white border rounded-lg max-w-full border-gray-200 leading-none w-auto p-2 pr-12 md:text-sm"
           type="number"
           placeholder="From"
           min="0" />
-        <label for="price-from" class="leading-none px-2 text-gray-400 absolute" v-html="currencySymbol" />
+        <label for="price-from" class="leading-none px-2 text-gray-400 absolute right-0" v-html="currencySymbol" />
       </div>
       <div class="flex relative items-center">
         <input
           id="price-to"
           v-model="price[1]"
-          class="bg-white border rounded-lg max-w-full border-gray-200 leading-none w-auto p-2 pl-6 md:text-sm"
+          class="bg-white border rounded-lg max-w-full border-gray-200 leading-none w-auto p-2 pr-12 md:text-sm"
           type="number"
           placeholder="Up to"
           min="1" />
-        <label for="price-to" class="leading-none px-2 text-gray-400 absolute" v-html="currencySymbol" />
+        <label for="price-to" class="leading-none px-2 text-gray-400 absolute right-0" v-html="currencySymbol" />
       </div>
       <div class="mx-1 mt-1 col-span-full">
         <ClientOnly>
