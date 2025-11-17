a35fa050e03ba59f350ed7bc829b682a63a149ea
7onc1 <anton.demirev84@gmail.com>
2025-11-17 10:42:30 +0200

    New redirect

--- woonuxt_base/redirects.config.ts
+++ woonuxt_base/redirects.config.ts
@@ -3,4 +3,8 @@ export default [
   { from: '/product/:slug', to: '/produkt/:slug', statusCode: 301 },
   { from: '/products', to: '/magazin', statusCode: 301 },
   { from: '/products/page/:page', to: '/magazin/page/:page', statusCode: 301 },
+  
+  // Редирект от стара категория "фитнес-уреди" към начална страница
+  { from: '/фитнес-уреди', to: '/', statusCode: 301 },
+  { from: '/фитнес-уреди/', to: '/', statusCode: 301 },
 ];
